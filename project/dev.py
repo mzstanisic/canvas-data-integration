44b01cd244f31684e17c964380c2dc232019e43c
mzstanisic <mzstanisic@gmail.com>
2024-07-29 10:04:38 -0400

    project structure

---
+++ project/dev.py
@@ -0,0 +1,49 @@
+import yaml
+import os
+import glob
+
+# test out yaml
+CONFIG_DIR = os.path.dirname(__file__) + '/../config.yml'
+# CONFIG_DIR = '/../config.yml'
+config = yaml.safe_load(open(CONFIG_DIR))
+
+print(config)
+
+prefix_path: str = os.path.dirname(__file__) + config['prefix_path'] 
+final_path: str = os.path.dirname(__file__) + config['final_path']
+
+print(prefix_path)
+print(final_path)
+
+print(os.listdir(prefix_path))
+print(os.listdir(final_path))
+
+# clear temp files from subfolders
+for temps in glob.glob(os.path.join(prefix_path, "**"), recursive=True):
+    if temps.endswith((".csv", ".gz")):
+        os.remove(temps)
+
+# check local machine doesn't have environment variables
+# base_url: str = os.environ["DAP_API_URL"]
+# client_id: str = os.environ["DAP_CLIENT_ID"]
+# client_secret: str = os.environ["DAP_CLIENT_SECRET"]
+
+# base_url
+# client_id
+# client_secret
+
+
+# test out dotenv library for local development
+# from dotenv import load_dotenv, find_dotenv
+
+# # load_dotenv() # should search parent directory by default
+# # load_dotenv(find_dotenv()) # try this one if it doesn't
+# load_dotenv(os.path.dirname(__file__) + '/../resources/.env')
+
+# dap_api_url: str = os.environ.get("DAP_API_URL")
+# dap_client_id: str = os.environ.get("DAP_CLIENT_ID")
+# dap_client_secret: str = os.environ.get("DAP_CLIENT_SECRET")
+
+# print(dap_api_url)
+# print(dap_client_id)
+# print(dap_client_secret)
